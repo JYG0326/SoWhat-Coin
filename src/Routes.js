ff75b36edf1c157b53718ea9036678723170461f
JYG <jojeayun900@gmail.com>
2021-12-13 18:21:57 +0900

    setting Routes

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./Main";
+
+export default class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </Router>
+    );
+  }
+}
