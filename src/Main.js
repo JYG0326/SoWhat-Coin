15b9e03a97c64ee16dbf57a95d33f38a2c6fe424
JYG <jojeayun900@gmail.com>
2021-12-13 18:20:19 +0900

    rename

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+
+function Main() {
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        
+      </main>
+      <Footer />
+    </div>
+    //여기까지 JSX
+  );
+}
+
+export default Main;
