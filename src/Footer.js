322415b3566751c9604ee9be2b918edded78c69a
JYG <jojeayun900@gmail.com>
2021-12-14 15:49:26 +0900

    make frame of Header&Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,13 @@
 import './App.css';
 
-function Header() {
+function Footer() {
   return (
     <footer>
-
+      <img className='F_logo' alt="어쩔코인_로고" src="./logo.png" />
+      <p>Copyright 2021. SoWhatCoin All rights reserved.</p>
+      <p>MAIL: s00000@gsm.hs.kr</p>
     </footer>
   );
 }
 
-export default Header;
+export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,19 @@ import './App.css';
 function Header() {
   return (
     <header>
-        <nav>
-            <div><img src="./logo.png" /></div>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </nav>
+      <nav className='H_nav'>
+        <div className='logo_interface'><img alt='어쩔코인_로고' src="./logo.png" /></div>
+        <ul className='function_interface'>
+          <li>거래소</li>
+          <li>투자내역</li>
+          <li>코인동향</li>
+        </ul>
+        <ul className='login_interface'>
+          <li>로그인</li>
+          <li>회원가입</li>
+        </ul>
+      </nav>
+
     </header>
   );
 }
