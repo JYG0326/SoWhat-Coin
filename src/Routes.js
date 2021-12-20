b5c2c0d67e055e702386d84e97758d23650c9760
JYG <jojeayun900@gmail.com>
2021-12-20 21:09:18 +0900

    connect exchange page with routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Main/Main";
+import Exchange from "./Exchange/Exchange";
 
 export default class Routes extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export default class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/Exchange" component={Exchange} />
         </Switch>
       </Router>
     );
