e0f872f4316ae3db8c6c06e09dcaff6b2de89c56
JYG <jojeayun900@gmail.com>
2021-12-23 21:16:43 +0900

    modify text design

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -21,7 +21,7 @@ function Main() {
       <main>
         <img alt='배경화면' src='./background.png' />
         <div className='textbox'>
-          <h1>GSM의 소식을 누구보다 빨리 접할 수 있는 디지털 자산 거래소</h1>
+          <h1>GSM의 소식을 누구보다 빨리 접할 수 있는 사이트</h1>
           <p>안전하고 투명한 시스템으로 빠르고 정확한 정보들을 제공합니다</p>
         </div>
         <div className='nowcoins'>
