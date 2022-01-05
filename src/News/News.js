3cd3925e1a2844af98bfb0c8b8ff0995b86e8463
JYG <jojeayun900@gmail.com>
2022-01-05 20:57:39 +0900

    connect to server & make function of post/get data

--- src/News/News.js
+++ src/News/News.js
@@ -1,46 +1,105 @@
 import './News.css';
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 function News() {
-  
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        <div className='N_contents1'>
-          <h2>글 목록</h2>
-          <div className='writing_list'>
-            <div><p className='list_title'>GSM 기숙사 폭팔! 머무를 곳 잃은 학생들... 학교가 발칵 뒤집혔다.</p><span className='uptime'>1분 전</span></div>
-            <div><p className='list_title'>GSM 로켓 1호 발사, 김희철 교장 "우주까지 개척해 광소마의 위상을 널리 알릴것"</p><span className='uptime'>2시간 전</span></div>
-            <div><p className='list_title'>GSM 기숙사 폭팔! 머무를 곳 잃은 학생들... 학교가 발칵 뒤집혔다.</p><span className='uptime'>7시간 전</span></div>
-            <div><p className='list_title'>GSM 로켓 1호 발사, 김희철 교장 "우주까지 개척해 광소마의 위상을 널리 알릴것"</p><span className='uptime'>12시간 전</span></div>
-            <div><p className='list_title'>GSM 기숙사 폭팔! 머무를 곳 잃은 학생들... 학교가 발칵 뒤집혔다.</p><span className='uptime'>14시간 전</span></div>
-            <div><p className='list_title'>GSM 로켓 1호 발사, 김희철 교장 "우주까지 개척해 광소마의 위상을 널리 알릴것"</p><span className='uptime'>17시간 전</span></div>
-            <div><p className='list_title'>GSM 기숙사 폭팔! 머무를 곳 잃은 학생들... 학교가 발칵 뒤집혔다.</p><span className='uptime'>23시간 전</span></div>
-            <div><p className='list_title'>GSM 로켓 1호 발사, 김희철 교장 "우주까지 개척해 광소마의 위상을 널리 알릴것"</p><span className='uptime'>1일 전</span></div>
-            <div><p className='list_title'>GSM 기숙사 폭팔! 머무를 곳 잃은 학생들... 학교가 발칵 뒤집혔다.</p><span className='uptime'>1일 전</span></div>
-            <div><p className='list_title'>GSM 로켓 1호 발사, 김희철 교장 "우주까지 개척해 광소마의 위상을 널리 알릴것"</p><span className='uptime'>1일 전</span></div>
-            <div><p className='list_title'>GSM 기숙사 폭팔! 머무를 곳 잃은 학생들... 학교가 발칵 뒤집혔다.</p><span className='uptime'>3일 전</span></div>
-            <div><p className='list_title'>GSM 로켓 1호 발사, 김희철 교장 "우주까지 개척해 광소마의 위상을 널리 알릴것"</p><span className='uptime'>7일 전</span></div>
-          </div>
-          <p>※코인동향에 올라온 일부 글들은 실제 코인시세에 영향을 미칠 수 있습니다※</p>
-        </div>
-        <div className='N_contents2'>
-          <h2>내가 보았거나 알고있는 재미있는 소식을 코인으로 만들고 싶다면?</h2>
-          <div>
-            <input type='text' className='input_title' placeholder='소식의 제목을 입력해주세요!' />
-            <br />
-            <textarea rows="25" className='input_text' cols="75" placeholder='제보할 내용을 입력해주세요!' />
-            <br />
-            <button className="btn">제보하기</button>
-          </div>
-        </div>
-      </main>
-      <Footer />
-    </div>
-    //여기까지 JSX
-  );
-}
 
-export default News;
+  const [data, setData] = useState([{ id: 1, title: '불러오는중', description: "불러오는중", tag: '불러오는중', create_at: "불러오는중" }])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await axios.get('/issue/getAll')
+      console.log(response.data)
+      setData(response.data)
+      console.log({ data })
+    }
+    fetchUsers()
+  }, [])
+
+  function postWrite() {
+    fetch('/issue/write', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: {title},
+        description: {description},
+        tag: "김동동코인",
+      }),
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        if (response === true) {
+          return alert('글 등록에 성공하였습니다!')
+        }
+
+        if (response !== true) {
+          return alert('글 등록에 실패하였습니다.')
+        }
+      })
+  }
+
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [tag, setTag] = useState("")
+
+    const handleTitle = e => {
+      const { value, name } = e.target
+      setTitle({[name]:value})
+    }
+
+    const handleDescription = e => {
+      const { value, name } = e.target
+      setDescription({[name]:value})
+    }
+
+    const handleTag = e => {
+      const { value, name } = e.target
+      setTag({[name]:value})
+    }
+
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <div className='N_contents1'>
+            <h2>글 목록</h2>
+            <div className='writing_list'>
+              {data.map(data => (
+                <div key={data.id} className="write_list">
+                  <h3 className='list_coin'>{data.tag}</h3>
+                  <div className="list_title">{data.title}</div>
+                  <span className='uptime'>{data.create_at}</span>
+                  <div>{data.description}</div>
+                </div>
+              ))}
+            </div>
+            <p>※코인동향에 올라온 일부 글들은 실제 코인시세에 영향을 미칠 수 있습니다※</p>
+          </div>
+          <div>
+          </div>
+          <div className='N_contents2'>
+            <h2>내가 보았거나 알고있는 재미있는 소식을 <br /> 코인으로 만들고 싶다면?</h2>
+            <div>
+              <input type='text' className='input_title' name="title" onChange={handleTitle} placeholder='소식의 제목을 입력해주세요!' />
+              <br />
+              <input type='text' className='input_title' name="title" onChange={handleTag} placeholder='관련된 코인을 입력해주세요!' />
+              <br />
+              <textarea rows="23" className='input_text' name="description" cols="75" onChange={handleDescription} placeholder='제보할 내용을 입력해주세요!' />
+              <br />
+              <button className="btn" onClick={postWrite}>제보하기</button>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </div>
+      //여기까지 JSX
+    );
+  }
+
+  export default News;
